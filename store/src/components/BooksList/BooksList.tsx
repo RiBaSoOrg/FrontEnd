0d9f6b9e0a238ff949c9187c11d3e8c7c72bd38f
sosophar <sophia.piyamit@student.htw-berlin.de>
2024-07-17 13:29:17 +0200

    cypress cleanup

--- store/src/components/BooksList/BooksList.tsx
+++ store/src/components/BooksList/BooksList.tsx
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from 'react';
 import './BooksList.css';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';

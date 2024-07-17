bd4a3585ebe0678ba2d087d49b24a9cdef7f25b3
Rico <rico.flemming@student.htw-berlin.de>
2024-07-17 18:03:34 +0200

    andere clientID test

--- store/src/Keycloak/keycloak-config.ts
+++ store/src/Keycloak/keycloak-config.ts
@@ -4,7 +4,7 @@ import Keycloak from 'keycloak-js';
 const keycloakConfig = {
     url: 'https://keycloak:8080/auth',
     realm: 'ribaso',
-    clientId: 'userservice',
+    clientId: 'frontend',
 };
 
 const keycloak = new Keycloak(keycloakConfig);

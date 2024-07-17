edf3c9af671877542d1993fc034b33dafbe345fe
sosophar <sophia.piyamit@student.htw-berlin.de>
2024-07-17 14:21:57 +0200

    CI yml cypress add update

--- store/cypress.config.ts
+++ store/cypress.config.ts
@@ -5,5 +5,6 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'http://localhost:3000', // Update this to match your development server
   },
 });

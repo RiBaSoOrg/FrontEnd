641a6d9c2844651cc1563138795d0153d89d8049
Rico <rico.flemming@student.htw-berlin.de>
2024-07-18 11:32:49 +0200

    url für docker geändert test

--- store/src/keycloak.ts
+++ store/src/keycloak.ts
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js'
 
 const keycloak = new Keycloak({
-    url: 'http://localhost:8080/',
+    url: 'http://keycloak:8080/',
     realm: 'ribaso',
     clientId: 'frontend'
 });// Pass initialization options as required or leave blank to load from 'keycloak.json'
